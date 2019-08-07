1741e796388161cce1f0eb339e60cfbaf68b922e
IAGILY <61g4iagily@gmail.com>
2019-08-07 10:36:04 +0800

    87

--- config/Database.php
+++ config/Database.php
@@ -3,11 +3,7 @@ class Database {
     private $host = null;
     private $db_name = 'extension';
     private $username = 'root';
-<<<<<<< HEAD
-    private $password = 'iagily';
-=======
     private $password = '61g4iagily';
->>>>>>> a9c55924b2278279adf515aa9b72b4d19c1056e2
     private $conn;
 
     public function connect() {
